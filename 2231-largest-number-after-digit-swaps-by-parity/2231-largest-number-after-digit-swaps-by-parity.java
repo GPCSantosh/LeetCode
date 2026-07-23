6c9bc76bdceebe1ca7e467f3577178047b88f2cc
Santosh@CR7 <127501283+GPCSantosh@users.noreply.github.com>
2026-07-23 09:10:10 +0530

    Time: 2 ms (74.06%), Space: 42.5 MB (25.47%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
@@ -1,29 +1,25 @@
-import java.util.*;
+import java.util.PriorityQueue;
 import java.util.Collections;
 class Solution {
     public int largestInteger(int num) {
-        String str = Integer.toString(num);        
-        List<Integer> evens = new ArrayList<>();
-        List<Integer> odds = new ArrayList<>();
+        String str = Integer.toString(num);
+        PriorityQueue<Integer> evens = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> odds = new PriorityQueue<>(Collections.reverseOrder());
         for (char c : str.toCharArray()) {
             int digit = c - '0';
             if (digit % 2 == 0) {
-                evens.add(digit);
+                evens.offer(digit);
             } else {
-                odds.add(digit);
+                odds.offer(digit);
             }
         }
-        Collections.sort(evens, Collections.reverseOrder());
-        Collections.sort(odds, Collections.reverseOrder());
         StringBuilder result = new StringBuilder();
-        int evenIndex = 0;
-        int oddIndex = 0;      
         for (char c : str.toCharArray()) {
             int digit = c - '0';
             if (digit % 2 == 0) {
-                result.append(evens.get(evenIndex++));
+                result.append(evens.poll());
             } else {
-                result.append(odds.get(oddIndex++));
+                result.append(odds.poll());
             }
         }        
         return Integer.parseInt(result.toString());
