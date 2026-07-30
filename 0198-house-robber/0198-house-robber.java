8ce4430b739c430280042941ed4c87b6246ff4cf
Santosh@CR7 <127501283+GPCSantosh@users.noreply.github.com>
2026-07-30 13:21:41 +0530

    Time: 0 ms (100%), Space: 42.6 MB (88.44%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }        
+        int rob1 = 0;
+        int rob2 = 0;
+        for (int num : nums) {
+            int temp = Math.max(rob1 + num, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }        
+        return rob2;
+    }
+}
